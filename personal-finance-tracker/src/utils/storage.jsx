b7e608e9ae109be9faf37b100c8be06d5f6f2acd
Amit Kumar <kamit62054@gmail.com>
2026-04-04 23:10:49 +0530

    adding storage

--- personal-finance-tracker/src/utils/storage.jsx
+++ personal-finance-tracker/src/utils/storage.jsx
@@ -3,9 +3,11 @@ import { STORAGE_KEYS } from "./constants";
 const getData = (key) => JSON.parse(localStorage.getItem(key)) || [];
 const setData = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 
+// Users
 export const getUsers = () => getData(STORAGE_KEYS.USERS);
 export const setUsers = (users) => setData(STORAGE_KEYS.USERS, users);
 
+// Current User
 export const getCurrentUser = () =>
   JSON.parse(localStorage.getItem(STORAGE_KEYS.CURRENT_USER)) || null;
 export const setCurrentUser = (user) =>
@@ -13,11 +15,20 @@ export const setCurrentUser = (user) =>
 export const clearCurrentUser = () =>
   localStorage.removeItem(STORAGE_KEYS.CURRENT_USER);
 
-export const getExpenses = () => getData(STORAGE_KEYS.EXPENSES);
-export const setExpenses = (expenses) => setData(STORAGE_KEYS.EXPENSES, expenses);
+// Get user-specific key
+const userKey = (key) => {
+  const user = getCurrentUser();
+  return user ? `${key}_${user.id}` : key;
+};
 
-export const getIncomes = () => getData(STORAGE_KEYS.INCOMES);
-export const setIncomes = (incomes) => setData(STORAGE_KEYS.INCOMES, incomes);
+// Expenses (per user)
+export const getExpenses = () => getData(userKey(STORAGE_KEYS.EXPENSES));
+export const setExpenses = (expenses) => setData(userKey(STORAGE_KEYS.EXPENSES), expenses);
 
-export const getBudgets = () => getData(STORAGE_KEYS.BUDGETS);
-export const setBudgets = (budgets) => setData(STORAGE_KEYS.BUDGETS, budgets);
+// Incomes (per user)
+export const getIncomes = () => getData(userKey(STORAGE_KEYS.INCOMES));
+export const setIncomes = (incomes) => setData(userKey(STORAGE_KEYS.INCOMES), incomes);
+
+// Budgets (per user)
+export const getBudgets = () => getData(userKey(STORAGE_KEYS.BUDGETS));
+export const setBudgets = (budgets) => setData(userKey(STORAGE_KEYS.BUDGETS), budgets);
